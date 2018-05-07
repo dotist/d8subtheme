08a4f9901ee7cbc978d485bbf5637ec5f18604f5
Isaac Bigsby Trogdon <mr@dyss.net>
2018-05-07 10:53:36 +0200

    Add libraries file and resources.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+(function($) {
+
+  /**
+   * Object definition for displaySettings.
+   *
+   * @param {*}
+   */
+
+  Drupal.behaviors.gallery1_ui = {
+    attach: function (context, settings) {
+      console.log('gallery1_ui/scripts.js');
+    }
+  };
+  })(jQuery);
