cc047685ce3f4c0b6bf55c977b6ab2277ee9e52f
Isaac Bigsby Trogdon <mr@dyss.net>
2018-05-07 11:02:53 +0200

    Clean up the setup.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,9 @@
 
   Drupal.behaviors.gallery1_ui = {
     attach: function (context, settings) {
-      console.log('gallery1_ui/scripts.js');
+      $('body', context).once('gallery1_ui').each(function () {
+        console.log('gallery1_ui/scripts.js');
+      });
     }
   };
   })(jQuery);
